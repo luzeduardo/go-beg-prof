ea3f6635c7bb291c7fc23f289e7c355ab30acf2b
Eduardo Luz <luz.eduardo@gmail.com>
2024-06-12 17:29:03 +0100

    adding dummy translation

--- translation/translator.go
+++ translation/translator.go
@@ -1,5 +1,15 @@
 package translation
 
 func Translate(word string, language string) string {
+	switch language {
+	case "english":
+		return word
+	case "finnish":
+		return "hei"
+	case "german":
+		return "hallo"
+	case "dutch":
+		return ""
+	}
 	return word
 }
--- translation/translator_test.go
+++ translation/translator_test.go
@@ -14,4 +14,19 @@ func TestTranslate(t *testing.T) {
 	if res != "hello" {
 		t.Errorf(`expected "hello" but received "%s"`, res)
 	}
+
+	res = translation.Translate("hello", "german")
+	if res != "hallo" {
+		t.Errorf(`expected "hei" but received "%s"`, res)
+	}
+
+	res = translation.Translate("hello", "finnish")
+	if res != "hei" {
+		t.Errorf(`expected "hei" but received "%s"`, res)
+	}
+
+	res = translation.Translate("hello", "dutch")
+	if res != "" {
+		t.Errorf(`expected "hei" but received "%s"`, res)
+	}
 }
