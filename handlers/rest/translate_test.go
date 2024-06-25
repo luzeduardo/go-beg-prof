e36e8117b9646eb265e79062148895929182b717
Eduardo Luz <luz.eduardo@gmail.com>
2024-06-25 10:10:14 +0100

    stubbing the translator service

--- handlers/rest/translate_test.go
+++ handlers/rest/translate_test.go
@@ -7,9 +7,16 @@ import (
 	"testing"
 
 	"github.com/luzeduardo/shipping-go/handlers/rest"
-	"github.com/luzeduardo/shipping-go/translation"
 )
 
+type stubbedService struct{}
+
+func (s *stubbedService) Translate(word string, language string) string {
+	if word == "foo" {
+		return "bar"
+	}
+	return ""
+}
 func TestTranslateAPI(t *testing.T) {
 	tt := []struct {
 		Endpoint            string
@@ -19,15 +26,15 @@ func TestTranslateAPI(t *testing.T) {
 	}{
 		{
 			Endpoint:            "/hello",
-			StatusCode:          http.StatusOK,
-			ExpectedLanguage:    "english",
-			ExpectedTranslation: "hello",
+			StatusCode:          http.StatusNotFound,
+			ExpectedLanguage:    "",
+			ExpectedTranslation: "",
 		},
 		{
 			Endpoint:            "/hello?language=german",
-			StatusCode:          http.StatusOK,
-			ExpectedLanguage:    "german",
-			ExpectedTranslation: "hallo",
+			StatusCode:          http.StatusNotFound,
+			ExpectedLanguage:    "",
+			ExpectedTranslation: "",
 		},
 		{
 			Endpoint:            "/hello?language=notcoveredlanguage",
@@ -41,29 +48,35 @@ func TestTranslateAPI(t *testing.T) {
 			ExpectedLanguage:    "",
 			ExpectedTranslation: "",
 		},
+		{
+			Endpoint:            "/foo?language=english",
+			StatusCode:          http.StatusOK,
+			ExpectedLanguage:    "english",
+			ExpectedTranslation: "bar",
+		},
 	}
 	for _, test := range tt {
 		rr := httptest.NewRecorder()
 		req, _ := http.NewRequest("GET", test.Endpoint, nil)
 
-		underTest := rest.NewTranslatorHandler(translation.NewStaticService())
+		underTest := rest.NewTranslatorHandler(&stubbedService{})
 		handler := http.HandlerFunc(underTest.TranslateHandler)
 
 		handler.ServeHTTP(rr, req)
 
 		if rr.Code != test.StatusCode {
-			t.Errorf(`expected status %d but received %d`, test.StatusCode, rr.Code)
+			t.Errorf(`expected status %d but received %d on %s`, test.StatusCode, rr.Code, test.Endpoint)
 		}
 
 		var resp rest.Resp
 		json.Unmarshal(rr.Body.Bytes(), &resp)
 
 		if resp.Language != test.ExpectedLanguage {
-			t.Errorf(`expected language "%s" but received %s`, test.ExpectedLanguage, resp.Language)
+			t.Errorf(`expected language "%s" but received %s on %s`, test.ExpectedLanguage, resp.Language, test.Endpoint)
 		}
 
 		if resp.Translation != test.ExpectedTranslation {
-			t.Errorf(`expected Translation "%s" but received %s`, test.ExpectedTranslation, resp.Translation)
+			t.Errorf(`expected Translation "%s" but received %s on %s`, test.ExpectedTranslation, resp.Translation, test.Endpoint)
 		}
 	}
 }
