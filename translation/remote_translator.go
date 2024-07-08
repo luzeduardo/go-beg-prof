a34f239e191a78934a4542f8d6a2d8ec3da7c34f
Eduardo Luz <luz.eduardo@gmail.com>
2024-07-08 22:59:57 +0100

    uses in-memory cache

--- translation/remote_translator.go
+++ translation/remote_translator.go
@@ -1,6 +1,8 @@
 package translation
 
 import (
+	"fmt"
+	"log"
 	"strings"
 )
 
@@ -9,6 +11,7 @@ var __restTranslator = &RemoteService{}
 // Remote service for external calls
 type RemoteService struct {
 	client HelloClient
+	cache  map[string]string
 }
 
 type HelloClient interface {
@@ -16,12 +19,26 @@ type HelloClient interface {
 }
 
 func NewRemoteService(client HelloClient) *RemoteService {
-	return &RemoteService{client: client}
+	return &RemoteService{client: client,
+		cache: make(map[string]string),
+	}
 }
 
 func (s *RemoteService) Translate(word string, language string) (string, error) {
 	word = strings.ToLower(word)
 	language = strings.ToLower(language)
+
+	key := fmt.Sprintf("%s:%s", word, language)
+	tr, ok := s.cache[key]
+	if ok {
+		return tr, nil
+	}
+
 	resp, err := s.client.Translate(word, language)
+	if err != nil {
+		log.Println(err)
+		return "", err
+	}
+	s.cache[key] = resp
 	return resp, err
 }
